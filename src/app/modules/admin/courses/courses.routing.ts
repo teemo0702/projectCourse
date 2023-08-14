5c285212769ce6be4be3e315301d8eeaa296b813
teemo0702 <71584416+teemo0702@users.noreply.github.com>
2023-08-15 00:04:55 +0700

    up

--- src/app/modules/admin/courses/courses.routing.ts
+++ src/app/modules/admin/courses/courses.routing.ts
@@ -1,9 +1,33 @@
 import { Route } from '@angular/router';
 import {CoursesComponent} from './courses.component';
+import {FileManagerListComponent} from "../apps/file-manager/list/list.component";
+import {
+    FileManagerFolderResolver,
+    FileManagerItemResolver,
+    FileManagerItemsResolver
+} from "../apps/file-manager/file-manager.resolvers";
+import {FileManagerDetailsComponent} from "../apps/file-manager/details/details.component";
+import {CanDeactivateFileManagerDetails} from "../apps/file-manager/file-manager.guards";
 
 export const coursesRoutes: Route[] = [
     {
         path: '',
         component: CoursesComponent,
+        // children : [
+        //     {
+        //         path     : 'folders/:folderId',
+        //         component: FileManagerListComponent,
+        //         children : [
+        //             {
+        //                 path         : 'details/:id',
+        //                 component    : FileManagerDetailsComponent
+        //             }
+        //         ]
+        //     },
+        //     {
+        //         path     : '',
+        //         component: FileManagerListComponent
+        //     }
+        // ]
     },
 ];
